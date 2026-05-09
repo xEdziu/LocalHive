b25f9c91faa93baaadbf7ac090eefd6bf173380f
Adrian Goral <adrian.goral@gmail.com>
2026-05-09 12:49:37 +0200

    chore: setup backend and frontend commit

---
+++ localhive-backend/src/main/java/dev/adrian/goral/localhivebackend/LocalhiveBackendApplication.java
@@ -0,0 +1,13 @@
+package dev.adrian.goral.localhivebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalhiveBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LocalhiveBackendApplication.class, args);
+    }
+
+}
